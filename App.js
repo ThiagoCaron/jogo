59f72bd1b42b3761079c734ce27be52d1cccaff1
ThiagoCaron <thiagocaron@outlook.com>
2023-05-03 21:14:06 -0300

    Teste para acessar a camera

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  console.log(permission);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>O aplicativo eu já estou editando, mas cadê a Steice que não vem logo?!?!</Text>
+      <Button title='Abri camera' onPress={requestPermission} />
+      <Camera style={styles.camera} type={CameraType.front}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +25,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    width: 200,
+    height: 400,
+  }
 });
